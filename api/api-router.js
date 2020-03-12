52b0787e7885a656e6192a226ebdbe8f39dfe47b
Sunil Karki <karkisunil1200@gmail.com>
2020-03-12 12:03:03 -0500

    bug fixed

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,9 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/', (req, res) => {
-  res.status(200).json({ api: 'up' });
+  const enviroment = process.env;
+  const prot = process.env.PORT;
+  res.status(200).json({api: 'up'});
 });
 
 router.get('/shouts', (req, res, next) => {
